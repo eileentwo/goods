39049209fab9c00a0ad166452dc9bcc4dab75fe9
eileentwo <578194597@qq.com>
2019-08-14 13:34:57 +0800

    index

---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: ['../../images/cake.png','../../images/cake.png'],
+    currentSwiper:0,
+    nav1: ['美食', '美食', '美食', '美食', '美食'],
+    goodsItem1: ['李记自助餐厅', '李记自助餐厅', '李记自助餐厅'],
+    goodsItem2: ['李记自助餐厅', '李记自助餐厅', '李记自助餐厅'],
+
+  },
+  onLoad: function () {
+  
+  },
+  
+})
